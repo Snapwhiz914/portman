ad31edff5948a39e6299a7fccba52cf92261ce51
Snapwhiz914 <asilversind@gmail.com>
2023-02-09 11:04:47 -0500

    test fixes

--- portman/subsystems/config.py
+++ portman/subsystems/config.py
@@ -16,7 +16,7 @@ def get_config_object():
         file_obj = open("/etc/portman.yaml", "r")
         result = yaml.safe_load(file_obj)
         validated = conf_schema.validate(result)
-        if not (os.path.exists(validated["nginx_site_loc"]) and os.path.exists(validated["cert_loc"])):
+        if not (os.path.isfile(validated["nginx_site_loc"]) and os.path.isdir(validated["cert_loc"])):
             raise Exception("nginx_site_loc or cert_loc has an invalid path. Make sure nginx_site_loc points to a nginx config file and cert_loc points to a directory with .pem files")
         return validated
     except FileNotFoundError as e:
